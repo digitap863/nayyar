2d50b8563bd659380509366a66ff50e533d3fd4a
feminafiroz <feminafiroz123@gmail.com>
2026-02-19 11:13:59 +0530

    feat: Configure API base URL based on environment and create Axios client instance.

--- client/src/api/url.js
+++ client/src/api/url.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = import.meta.env.MODE === "development" ? 'http://localhost:3000/api' : 'https://nayyar-r5cm.vercel.app/api'
+export const url = import.meta.env.MODE === "development" ? 'http://localhost:3000/api' : 'https://nayyar-1.onrender.com/api'
 export const AxiosAdmin = axios.create({
     baseURL: url
 });
